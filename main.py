bd135639a94a3dcc4a476d9f72e3cb4c2ccc4dce
Patrick Gio <108914351+ioSoyPato@users.noreply.github.com>
2024-05-19 01:54:30 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -103,5 +103,5 @@ async def user():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=4444)
+    uvicorn.run(app, host="127.0.0.1", port=4444, timeout_keep_alive=120)
 
