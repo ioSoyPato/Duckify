e41a32da1acc671b74c682ce9125c6e9ebe7fdb6
Patrick Gio <108914351+ioSoyPato@users.noreply.github.com>
2024-05-22 10:10:52 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,45 +2,86 @@
 import pandas as pd
 import re as r
 import random
+import librosa
+import matplotlib.pyplot as plt
 
+SONGS_DB = pd.read_csv("DataBase/SONGS_DB.csv")
+SONGS_DB.set_index("ID", inplace=True)
 
-ROCK = pd.read_csv("DataBase/ROCK.csv")
-ROCK["Genre"] = "ROCK" 
-ROCK["id"] = ROCK["id"].str.replace("ROCK","")
+# Drop all Unnameds
+Unnamed = [0,0.1,0.2,0.3]
 
-ELECTRONIC = pd.read_csv("DataBase/ELECTRONIC.csv")
-ELECTRONIC["Genre"] = "ELECTRONIC" 
-ELECTRONIC["id"] = ELECTRONIC["id"].str.replace("ELECTRONIC","")
-
-POP = pd.read_csv("DataBase/POP.csv")
-POP["Genre"] = "POP" 
-POP["id"] = POP["id"].str.replace("POP","")
-
-RAP = pd.read_csv("DataBase/RAP.csv")
-RAP["Genre"] = "RAP" 
-RAP["id"] = RAP["id"].str.replace("RAP","")
-
-# Concatenar todos los DataFrame
-SONGS_DB = pd.concat([ROCK, ELECTRONIC, POP, RAP])
-
-SONGS_DB.drop("Unnamed: 0", axis=1, inplace=True)
-SONGS_DB.drop("id",axis=1,inplace=True)
-
-SONGS_DB["ID"] = range(1,len(SONGS_DB)+1)
-
+try:
+    for i in Unnamed:
+        SONGS_DB.drop(f"Unnamed: {i}",axis=1, inplace=True)
+except:
+    print("Already clean")
+    
 currented_played = []
+def update_times_played(path:str):
+    SONGS_DB.loc[SONGS_DB["shortedPath"]==path, "TimesPlayed_Global"] += 1
+    SONGS_DB.to_csv("DataBase/SONGS_DB.csv")
+    print(f"Updated: {path}")
+
+def graphic_spectogram(path1:str, path2:str):
+    path1 = f"Playlist/{path1}"
+    title1 = r.split("/",path1)[-1]
+    path2 = f"Playlist/{path2}"
+    title2 = r.split("/",path2)[-1]
+    audio, sr = librosa.load(path1, sr=None)
+    D1 = librosa.amplitude_to_db(abs(librosa.stft(audio))) #Spectogram
+    audio, sr = librosa.load(path2, sr=None)
+    D2 = librosa.amplitude_to_db(abs(librosa.stft(audio))) #Spectogram
+
+    # Graphic both spectograms
+    fig, ax = plt.subplots(1, 2, figsize=(20, 5))
+    ax[0].imshow(D1, aspect='auto', origin='lower')
+    ax[0].set_title(title1)
+    ax[0].set_xlabel("Time")
+    ax[0].set_ylabel("Frequency")
+    ax[1].imshow(D2, aspect='auto', origin='lower')
+    ax[1].set_title(title2)
+    ax[1].set_xlabel("Time")
+    ax[1].set_ylabel("Frequency")
+    
+    # Save the figure as a png
+    plt.savefig("Home_Page2/spects/spectograms.png")
+
+    return {"title1":title1, "title2":title2}
+
+# def recommend_song(path:str, genre:str):
+#     currented_played.append(path)
+#     randipity = random.randint(0,6)
+#     if randipity >=3:
+#         current_genre = SONGS_DB[SONGS_DB["Genre"]==genre].head(1).Genre.unique()[0]
+#         recommendation = SONGS_DB[~SONGS_DB["shortedPath"].isin(currented_played)]
+#         recommendation = recommendation[recommendation["Genre"]== current_genre].head(1).shortedPath.unique()[0]
+#     else:
+#         current_genre = SONGS_DB[SONGS_DB["Genre"]!=genre].Genre.unique()[random.randint(0,2)]
+#         recommendation = SONGS_DB[~SONGS_DB["shortedPath"].isin(currented_played)]
+#         recommendation = recommendation[recommendation["Genre"]== current_genre].head(1).shortedPath.unique()[0]
+    
+#     recommendation = recommendation.split("/")
+    
+#     return {"genre":recommendation[2], "artist":recommendation[3], "title":recommendation[4]}
+
 def recommend_song(path:str, genre:str):
     currented_played.append(path)
     randipity = random.randint(0,6)
+    update_times_played(path)
     if randipity >=3:
         current_genre = SONGS_DB[SONGS_DB["Genre"]==genre].head(1).Genre.unique()[0]
         recommendation = SONGS_DB[~SONGS_DB["shortedPath"].isin(currented_played)]
-        recommendation = recommendation[recommendation["Genre"]== current_genre].head(1).shortedPath.unique()[0]
+        recommendation = recommendation[recommendation["Genre"]== current_genre].sort_values("TimesPlayed_Global",ascending=False).head(1).shortedPath.unique()[0]
     else:
         current_genre = SONGS_DB[SONGS_DB["Genre"]!=genre].Genre.unique()[random.randint(0,2)]
         recommendation = SONGS_DB[~SONGS_DB["shortedPath"].isin(currented_played)]
-        recommendation = recommendation[recommendation["Genre"]== current_genre].head(1).shortedPath.unique()[0]
+        recommendation = recommendation[recommendation["Genre"]== current_genre].sort_values("TimesPlayed_Global",ascending=False).head(1).shortedPath.unique()[0]
     
+    currented_played.append(recommendation)
+    update_times_played(recommendation)
+    # print(f"User History: {currented_played}") # Todays user history
+    # print(graphic_spectogram(path,recommendation))
     recommendation = recommendation.split("/")
     
     return {"genre":recommendation[2], "artist":recommendation[3], "title":recommendation[4]}
@@ -108,6 +149,7 @@ async def home():
 async def musicHome(request: Request, genre: str, artist: str, title:str):
     artist_show = cleaned_data(artist, genre, title)[0]
     title_Show = cleaned_data(artist, genre, title)[1]
+    update_times_played(f"/tracks/{genre}/{artist}/{title}")
     if True:
         return templates2.TemplateResponse(
         request=request, name="index.html", context={"title": title, "artist": artist, "genre": genre, "id": id, "title_show":title_Show, "artist_show":artist_show}
